578ae527fb4a725e4033a0472937be1d2c04bdb3
Avinaba-Ray <avinabaray@gmail.com>
2020-06-15 19:55:19 +0530

    ClientUI added

---
+++ src/com/avinabaray/chatapp/Client/ClientUI.java
@@ -0,0 +1,13 @@
+package com.avinabaray.chatapp.Client;
+
+import javax.swing.*;
+
+public class ClientUI {
+    private JTextArea messageText;
+    private JButton sendButton;
+    private JTextArea textArea1;
+    private JPanel clientPanel;
+    private JTextField textField1;
+    private JButton CONNECTButton;
+    private JButton DISCONNECTButton;
+}
